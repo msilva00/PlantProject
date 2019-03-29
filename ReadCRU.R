65db0d0942405432568c46a5414b72775ee64963
Mary Silva <yramsilva@eduroam-169-233-140-117.ucsc.edu>
2019-03-29 14:58:16 -0700

    Update R code for reading CRU

--- ReadCRU.R
+++ ReadCRU.R
@@ -1,15 +1,100 @@
 library(chron)
 library(RColorBrewer)
 library(lattice)
-library(ncdf)
+library(ncdf4)
 
 
-setwd("~/MasterProject/output_082188b2fa44421a4ac7e5c7b8fa2d1d_20190329_204733")
+setwd("~/MasterProject/CRU_Datasets/")
 
-ncname <- "tmp_cru-ts-4.02-gridded_124.75w32.75n113.25w42.25n_19010116-19011216"
-ncfname <- paste(ncname, ".nc", sep = "")
-dname <- "tmp"  # note: tmp means temperature (not temporary)
+temp = list.files(pattern="*.nc")
+# Loop over files
+for(i in seq_along(temp)) {
+  ncin = nc_open(temp[i])
+}
 
 # open a NetCDF file
-ncin <- nc_open(ncfname)
 print(ncin)
+
+# Get longitude values
+lon <- ncvar_get(ncin,"lon")
+nlon <- dim(lon)
+head(lon)
+
+# Get latitude values
+lat <- ncvar_get(ncin,"lat")
+nlat <- dim(lat)
+head(lat)
+
+
+print(c(nlon,nlat))
+
+# get time
+time <- ncvar_get(ncin,"time")
+time
+tunits <- ncatt_get(ncin,"time","units")
+nt <- dim(time)
+nt
+tunits
+
+# get temperature
+tmp_array <- ncvar_get(ncin,dname)
+dlname <- ncatt_get(ncin,dname,"long_name")
+dunits <- ncatt_get(ncin,dname,"units")
+fillvalue <- ncatt_get(ncin,dname,"_FillValue")
+dim(tmp_array)
+
+# get global attributes
+title <- ncatt_get(ncin,0,"title")
+institution <- ncatt_get(ncin,0,"institution")
+datasource <- ncatt_get(ncin,0,"source")
+references <- ncatt_get(ncin,0,"references")
+history <- ncatt_get(ncin,0,"history")
+Conventions <- ncatt_get(ncin,0,"Conventions")
+
+
+ls()
+# load some packages
+library(chron)
+library(lattice)
+library(RColorBrewer)
+
+# convert time -- split the time units string into fields
+tustr <- strsplit(tunits$value, " ")
+tdstr <- strsplit(unlist(tustr)[3], "-")
+tmonth <- as.integer(unlist(tdstr)[2])
+tday <- as.integer(unlist(tdstr)[3])
+tyear <- as.integer(unlist(tdstr)[1])
+chron(time,origin=c(tmonth, tday, tyear))
+
+# replace netCDF fill values with NA's
+tmp_array[tmp_array==fillvalue$value] <- NA
+
+length(na.omit(as.vector(tmp_array[,,1])))
+
+# get a single slice or layer (January)
+m <- 1
+tmp_slice <- tmp_array[,,m]
+
+# quick map
+image(lon,lat,tmp_slice, col=rev(brewer.pal(10,"RdBu")))
+
+# levelplot of the slice
+grid <- expand.grid(lon=lon, lat=lat)
+cutpts <- c(-50,-40,-30,-20,-10,0,10,20,30,40,50)
+levelplot(tmp_slice ~ lon * lat, data=grid, at=cutpts, cuts=11, pretty=T, 
+          col.regions=(rev(brewer.pal(10,"RdBu"))))
+
+# create dataframe -- reshape data
+# matrix (nlon*nlat rows by 2 cols) of lons and lats
+lonlat <- as.matrix(expand.grid(lon,lat))
+dim(lonlat)
+
+# vector of `tmp` values
+tmp_vec <- as.vector(tmp_slice)
+length(tmp_vec)
+
+# create dataframe and add names
+tmp_df01 <- data.frame(cbind(lonlat,tmp_vec))
+names(tmp_df01) <- c("lon","lat",paste(dname,as.character(m), sep="_"))
+head(na.omit(tmp_df01), 15)
+
